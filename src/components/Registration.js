6cf5b0c269ccfcd2b163dd870dba67dd61e38064
harshitkumar712 <harshitkumar712@gmail.com>
2020-11-10 19:20:31 +0530

    removed warning

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Alert, Form, Button } from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
 
 import '../styles/registration.css';
 
